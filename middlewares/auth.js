dd76bd291e68550ba5fc5771dacd1ff8f1617008
Bree Massingill <breemassingill@gmail.com>
2024-10-23 09:43:46 -0500

    fix login error from last commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
+const JWT_SECRET = require("../utils/config");
 const { UnauthorizedError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
